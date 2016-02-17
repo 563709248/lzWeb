57ebf6ce506305577e87edc79a1a3ffddb68a41f
lailz <lailz@lordstar.net>
2016-02-17 15:55:07 +0800

    Initial commit

---
+++ src/main/java/com/lz/controller/HelloWord.java
@@ -0,0 +1,23 @@
+package com.lz.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Maven on 2016/2/17.
+ */
+@Component
+@RequestMapping(value = "/helloWord")
+public class HelloWord {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView index(Model model) {
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("title", "Spring MVC And Freemarker");
+        mv.addObject("content", " Hello world ， test my first spring mvc ! ");
+        return mv;
+    }
+}
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-
+<h2>Hello World view!</h2>
 </body>
 </html>
